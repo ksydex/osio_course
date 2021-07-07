1a90fb1bd7feb23cdab230a1e77511a2633cacef
Artyom Lukyanov <artglz63@gmail.com>
2021-07-07 13:22:34 +0300

    init

---
+++ osio/osio.cpp
@@ -0,0 +1,46 @@
+﻿#ifndef UNICODE
+#define UNICODE
+#endif 
+
+#include "framework.h"
+#include "resource.h"
+#include <windows.h>
+
+BOOL CALLBACK mainWindowHandle(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR strCmdLine, int nCmdShow)
+{
+	DialogBox(hInstance,
+		MAKEINTRESOURCE(IDD_MAIN),
+		NULL,
+		mainWindowHandle);
+
+	return 0;
+}
+
+/*---------------------------------------------------------------------------*/
+
+BOOL CALLBACK mainWindowHandle(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+		//case WM_INITDIALOG:
+		//	return SetDlgMsgResult(hWnd, uMsg, HANDLE_WM_INITDIALOG((hWnd), (wParam), (lParam), (DialogOnInitDialog)));
+
+		//case WM_COMMAND:
+		//	return SetDlgMsgResult(hWnd, uMsg, HANDLE_WM_COMMAND((hWnd), (wParam), (lParam), (DialogOnCommand)));
+
+		//case WM_TIMER:
+		//	return SetDlgMsgResult(hWnd, uMsg, HANDLE_WM_TIMER((hWnd), (wParam), (lParam), (DialogOnTimer)));
+
+	}
+
+	return FALSE;
+
+}
+
+/*---------------------------------------------------------------------------*/
---
+++ osio/resource.h
@@ -0,0 +1,39 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется osio.rc
+//
+#define IDC_MYICON                      2
+#define IDD_B_DELETE                    3
+#define IDD_B_COPY                      4
+#define IDD_B_ATTRS                     5
+#define IDD_B_INFO                      6
+#define IDD_B_LINK                      7
+#define IDCANCEL7                       8
+#define IDD_B_RENAME                    8
+#define IDD_OSIO_DIALOG                 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_OSIO                        107
+#define IDI_SMALL                       108
+#define IDD_MAIN                        108
+#define IDC_OSIO                        109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDC_EDIT1                       1000
+#define IDD_B_CREATE                    1001
+#define IDD_B_EDIT                      1002
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
